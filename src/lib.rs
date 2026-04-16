94a6297a9b659797d876ef14ae2cef9d84263099
Andrew Liang <alian24@illinois.edu>
2026-04-16 14:51:01 -0500

    Added decipher function and updated caesar function for the decipher logic

--- src/lib.rs
+++ src/lib.rs
@@ -8,17 +8,20 @@ impl VigenereCipher {
             key : key.to_string(),
         }
     }
+
     //Takes in a letter and shifts it by the shift letter
     //Assumption: Key and the input text is/ will end up as uppercase
-    pub fn caesar_shift(letter: char, shift_letter: char) -> char {
-        if !letter.is_ascii_alphabetic() {
-            return letter;
-        }
+    pub fn caesar_shift(letter: char, shift_letter: char, encrypt: bool) -> char {
         let shift = shift_letter.to_ascii_uppercase() as u8 - b'A';
         let base = b'A';
-        let shifted = (((letter as u8 + shift - base) % 26) + base) as char;
-        return shifted;
+        // If encrypt is false, then we will "decipher" the letter
+        let shift  = if encrypt {
+            (letter as u8 + shift - base) % 26
+        } else {
+            (letter as u8 - shift - base + 26) % 26
+        };
 
+        (shift + base) as char
     }
     
     pub fn vigenere_encrypt(&self, original_message: &str) -> String {
@@ -35,7 +38,7 @@ impl VigenereCipher {
 
                 // turns c upper case to work with caesar_shift;
                 let c_upper = c.to_ascii_uppercase();
-                let shifted_char = Self::caesar_shift(c_upper, shift_letter);
+                let shifted_char = Self::caesar_shift(c_upper, shift_letter, true);
                 encrypted_message.push(shifted_char);
                 //Key index wraps back if it reaches end of key
                 key_idx = (key_idx + 1) % key_iter.len();
@@ -44,5 +47,30 @@ impl VigenereCipher {
             }
         }
         encrypted_message
+    }
+
+    pub fn vigenere_decipher(&self, original_message: &str) -> String {
+        let mut decipered_message: String = Default::default();
+        // Filters out non ascii characters and non letters (numbers, spaces, ect.)
+        // Then makes the characters uppercase to work with the caesar_shift function
+        let key_iter: Vec<char>  = self.key.chars().filter(|c| c.is_ascii_alphabetic()).map(|c| c.to_ascii_uppercase()).collect();
+
+        let mut key_idx = 0;
+        // Iterates through the original_message characters running caeser_shift for all the valid chars
+        for c in original_message.chars() {
+            if c.is_ascii_alphabetic() {
+                let shift_letter = key_iter[key_idx];
+
+                // turns c upper case to work with caesar_shift;
+                let c_upper = c.to_ascii_uppercase();
+                let shifted_char = Self::caesar_shift(c_upper, shift_letter, false);
+                decipered_message.push(shifted_char);
+                //Key index wraps back if it reaches end of key
+                key_idx = (key_idx + 1) % key_iter.len();
+            } else {
+                decipered_message.push(c);
+            }
+        }
+        decipered_message
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,17 @@ fn main() {
     let cipher : VigenereCipher = VigenereCipher::new(&key);
 
     //Shift by the letter 'A' should shift it by 0 letters and return 'A'
-    let caesar_shifted = VigenereCipher::caesar_shift('A','A');
-    println!("{}", caesar_shifted);
-    //shift by Z should shift it by 25 letters, resulting in 'Y'
-    let caesar_shifted = VigenereCipher::caesar_shift('A','Z');
-    println!("{}", caesar_shifted);
+    let caesar_shifted = VigenereCipher::caesar_shift('A','A', true);
+    println!("Original letter: A, Caesar shifted letter: {}", caesar_shifted);
+    //shift by Z should shift it by 25 letters, resulting in 'Z'
+    let caesar_shifted = VigenereCipher::caesar_shift('A','Z', true);
+    println!("Original letter: A, Caesar shifted letter: {}", caesar_shifted);
 
-    let original_text = "HELLO WORLD!";
+    let original_text = "Hello world!";
     // should return HFLNO ZOSLD! 
     let encrypted  = cipher.vigenere_encrypt(original_text);
-    print!("Orignal Word: {} Encrypted Word: {}", original_text, encrypted);
+    println!("Orignal Word: {} Encrypted Word: {}", original_text, encrypted);
+    // should return HELLO WORLD!
+    let decipered = cipher.vigenere_decipher(encrypted.as_str());
+    println!("Orignal Word: {} Encrypted Word: {}", encrypted, decipered);
 }
